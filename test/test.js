a126f2e916d89298ce2e284bffa2d3d4d46f93ac
Bruno Cauet <brunocauet@gmail.com>
2015-02-23 12:42:41 +0100

    Docu client library: load & store nodes, tests

--- test/test.js
+++ test/test.js
@@ -92,3 +92,66 @@ describe('Docu', function() {
         assert.equal(url.hash, "#node1");
     });
 });
+
+var DocuDisplay = require("../use_docu.js");
+var jsdom = require("mocha-jsdom");
+describe('DocuDisplay', function() {
+    jsdom();
+
+    before(function() {
+        require("../test-dep/classList.js");
+    });
+
+    it('setup dom', function() {
+        var div = document.createElement("div");
+        DocuDisplay.prototype.setUpDOM(div);
+        assert.equal(div.innerHTML, '<div class="docu-left-nav"></div>' +
+                                    '<div class="docu-main"></div>' +
+                                    '<div class="docu-right-nav"></div>')
+    });
+
+    it('constructor', function() {
+        var container = document.createElement("div");
+        var nodeLoader = function(node) {};
+        var dd = new DocuDisplay(container, {}, nodeLoader);
+        assert.equal(dd.nodeLoader, nodeLoader);
+        assert.equal(dd.container.outerHTML, '<div class="docu-main"></div>');
+        assert.equal(dd.container, container.children[1]);
+    });
+
+    it('navigate nodes', function() {
+        var container = document.createElement("div");
+        document.body.appendChild(container);
+
+        var data = {"nodes": {"node1": {"main": true, "picture": "hi.png"},
+                              "node2": {"main": true, "locationId": "deux"}},
+                    "root": "node1",
+                    "edges": [["node1", "node2"]]};
+        var nodeLoader = function(content) {
+            var text = document.createTextNode("loaded node with picture " +
+                                               content.picture);
+            this.container.appendChild(text);
+        };
+
+        var dd = new DocuDisplay(container, data, nodeLoader);
+        dd.loadNode(dd.docu.currentNode);
+        assert.equal(dd.container.innerHTML, "loaded node with picture hi.png");
+
+        assert.throws(function() { dd.goToNode(dd.docu.nodes["node2"]) },
+                      Error);
+
+        var deux = document.createElement("div");
+        deux.id = "deux";
+        deux.appendChild(document.createTextNode("deux deux"));
+        document.body.appendChild(deux);
+        dd.loadNode(dd.docu.nodes["node2"]);
+
+        assert.equal(dd.container.innerHTML, '<div id="deux">deux deux</div>');
+        assert.equal(document.body.lastChild.outerHTML,
+                     '<div id="docu-sentinel"></div>');
+
+        dd.goToNode(dd.docu.nodes["node1"]);
+        assert.equal(dd.container.innerHTML, "loaded node with picture hi.png");
+        assert.equal(document.getElementById("docu-sentinel"), null);
+    });
+});
--- use_docu.js
+++ use_docu.js
@@ -1,36 +1,69 @@
+"use strict";
+
 var Docu = require("./docu.js");
 
-function DocuDisplay(container, data) {
-    this.container = container;
+function DocuDisplay(container, data, nodeLoader) {
     this.docu = new Docu(data, window.location);
-    this.setUpDOM(container);
+    this.container = this.setUpDOM(container);
+    this.nodeLoader = nodeLoader;
 }
 
 DocuDisplay.prototype.setUpDOM = function(container) {
-    // put all needed tags in container
-    // FIXME
-}
+    var leftNav = document.createElement("div");
+    leftNav.classList.add("docu-left-nav");
+    var main = document.createElement("div");
+    main.classList.add("docu-main");
+    var rightNav = document.createElement("div");
+    rightNav.classList.add("docu-right-nav");
+    container.appendChild(leftNav);
+    container.appendChild(main);
+    container.appendChild(rightNav);
+    return main;
+};
 
-DocuDisplay.prototype.setUp = function() {
+DocuDisplay.setUp = function() {
     // static method that serves as a constructor
     // generate docu from #docu-display with its docu_data tag
-    var container = document.getElemetById("docu-display");
-    var data = required(container.docu_data);
+    var container = document.getElementById("docu-display");
+    var data = require(container.docu_data);
     var dd = new DocuDisplay(container, data);
-    dd.loadNode(dd.docu.current_node);
+    dd.loadNode(dd.docu.currentNode);
     return dd;
 };
 
 DocuDisplay.prototype.goToNode = function(node) {
-    this.container.setCurrentNode(node);
+    if(this.docu.currentNode.content.locationId) {
+        // restore it to its original location
+        var sentinel = document.getElementById("docu-sentinel");
+        sentinel.parentNode.insertBefore(this.container.childNodes[0], sentinel);
+        sentinel.parentNode.removeChild(sentinel);
+    } else {
+        // empty the container
+        while(this.container.hasChildNodes()) {
+            this.container.removeChild(this.container.lastChild);
+        }
+    }
+    this.docu.setCurrentNode(node);
     this.loadNode(node);
-}
+};
 
 DocuDisplay.prototype.loadNode = function(node) {
     if(node.content.locationId) {
-        // everything is in a node, copy its content in the main div?
-        // FIXME
+        // everything is in a node, copy its content in the docu-main div
+        // and put a mark on its original location
+        var sentinel = document.createElement("div");
+        sentinel.id = "docu-sentinel";
+        var content = document.getElementById(node.content.locationId);
+        if(content === null)
+            throw new Error("Cannot find element of id '" +
+                            node.content.locationId +
+                            "' for node " + node.name);
+        content.parentNode.insertBefore(sentinel, content);
+        this.container.appendChild(content);
     } else {
         // put picture, links, etc.
+        this.nodeLoader(node.content);
     }
-}
+};
+
+module.exports = DocuDisplay;
